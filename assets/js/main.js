90376db62cf0ec1466a839975ff0b28827c1431f
Pedro Vieira <86010036+pedrovs3@users.noreply.github.com>
2022-03-03 07:55:26 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,16 +35,16 @@ function getNivelImc(imc){
     const nivel = ['Abaixo do peso', 'Peso Normal', 'Sobrepeso', 'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3']
 
     if (imc >= 39.9){
-        return nivel[4];
+        return nivel[5];
     }
     if(imc > 34.9){
-        return nivel[3];
+        return nivel[4];
     }
     if(imc > 29.9){
-        return nivel[2];
+        return nivel[3];
     }
     if(imc > 24.9){
-        return nivel[1];
+        return nivel[2];
     }
     if(imc >= 18.5){
         return nivel[1];
